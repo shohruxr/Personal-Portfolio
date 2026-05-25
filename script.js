cb6de4dddf797a3a5f4a5e960085195965691a03
Shohrux <shohruhr85@gmail.com>
2026-05-26 02:13:35 +0900

    about css va javascript yozildi

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const minus = document.querySelector(".minus");
 const plus = document.querySelector(".plus");
 const reset = document.querySelector(".reset");
 
-let son = 0;
 minus.style.opacity = 0.3;
 minus.disabled = true;
+let son = 0;
 plus.addEventListener("click", function () {
   son++;
   counter.textContent = son;
@@ -28,5 +28,9 @@ reset.addEventListener("click", () => {
   minus.disabled = false;
 });
 
-// counter.value === 0;
-// minus.innerHTML = "<style>.minus:disabled{opacity:0; display: flex;padding: 6px;border-radius: 8px;background: #4a00e0;border: 1px solid #4a00e0;box-shadow: 0px 1px 2px 0px #0000000d;font-family: "Plus Jakarta Sans", sans-serif;font-weight: 600;font-size: 14px;line-height: 14px;letter-spacing: 0.7px;text-align: center;color: #ffffff;transition: 0.3s;}</style>";
+const input = document.querySelector(".input");
+const sanaydi = document.querySelector(".sanaydi");
+
+input.addEventListener("input", () => {
+  sanaydi.textContent = input.value.length;
+});
