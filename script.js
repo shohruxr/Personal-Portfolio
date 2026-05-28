f487b243ce1ea5873e68444a461ca8ba39a91aef
Shohrux <shohruhr85@gmail.com>
2026-05-29 02:43:01 +0900

    mini dastur xatolari va articledagi xatolar togirlandu

--- script.js
+++ script.js
@@ -4,19 +4,36 @@ const plus = document.querySelector(".plus");
 const reset = document.querySelector(".reset");
 const input = document.querySelector(".input");
 const sanaydi = document.querySelector(".sanaydi");
+let son = 0;
+if (minus) {
+  minus.style.opacity = 0.3;
+  minus.disabled = true;
+}
+
 if (plus) {
   plus.addEventListener("click", () => {
-    console.log("plus ishladi");
+    son++;
+    counter.textContent = son;
+    minus.style.opacity = 1;
+    minus.disabled = false;
   });
 }
 if (reset) {
   reset.addEventListener("click", () => {
-    console.log("reset ishladi");
+    son = 0;
+    counter.textContent = son;
+    minus.style.opacity = 0.3;
+    minus.disabled = true;
   });
 }
 if (minus) {
   minus.addEventListener("click", () => {
-    console.log("minus ishladi");
+    if (son >= 0) {
+      son--;
+      counter.textContent = son;
+      minus.style.opacity = 0.3;
+      minus.disabled = false;
+    }
   });
 }
 if (input && sanaydi) {
@@ -24,31 +41,3 @@ if (input && sanaydi) {
     sanaydi.textContent = input.value.length;
   });
 }
-
-minus.style.opacity = 0.3;
-minus.disabled = true;
-let son = 0;
-
-plus.addEventListener("click", function () {
-  son++;
-  counter.textContent = son;
-  minus.style.opacity = 1;
-  minus.disabled = false;
-});
-minus.addEventListener("click", function () {
-  if (son > 0) {
-    son--;
-    counter.textContent = son;
-  } else {
-    minus.style.opacity = 0.3;
-    minus.disabled = false;
-  }
-});
-reset.addEventListener("click", () => {
-  son = 0;
-  counter.textContent = son;
-  minus.style.opacity = 0.3;
-  minus.disabled = false;
-});
-
-console.log("nima");
