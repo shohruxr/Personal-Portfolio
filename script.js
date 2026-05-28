679292aaaaf710c1b3c1174f95a3aa2433828e2d
Shohrux <shohruhr85@gmail.com>
2026-05-28 22:10:43 +0900

    javascript toliq ulanib interaktiv mini funksiyalar tugatildi

--- script.js
+++ script.js
@@ -2,10 +2,33 @@ const counter = document.querySelector(".counterp");
 const minus = document.querySelector(".minus");
 const plus = document.querySelector(".plus");
 const reset = document.querySelector(".reset");
+const input = document.querySelector(".input");
+const sanaydi = document.querySelector(".sanaydi");
+if (plus) {
+  plus.addEventListener("click", () => {
+    console.log("plus ishladi");
+  });
+}
+if (reset) {
+  reset.addEventListener("click", () => {
+    console.log("reset ishladi");
+  });
+}
+if (minus) {
+  minus.addEventListener("click", () => {
+    console.log("minus ishladi");
+  });
+}
+if (input && sanaydi) {
+  input.addEventListener("input", function () {
+    sanaydi.textContent = input.value.length;
+  });
+}
 
 minus.style.opacity = 0.3;
 minus.disabled = true;
 let son = 0;
+
 plus.addEventListener("click", function () {
   son++;
   counter.textContent = son;
@@ -28,9 +51,4 @@ reset.addEventListener("click", () => {
   minus.disabled = false;
 });
 
-const input = document.querySelector(".input");
-const sanaydi = document.querySelector(".sanaydi");
-
-input.addEventListener("input", () => {
-  sanaydi.textContent = input.value.length;
-});
+console.log("nima");
